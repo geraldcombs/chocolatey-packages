f307175f1cbe5b30db92d75f5bbd1986f6cc15d3
Gerald Combs <gerald@wireshark.org>
2016-03-14 15:25:29 -0700

    Remove a comment.

--- apache-fop/tools/chocolateyuninstall.ps1
+++ apache-fop/tools/chocolateyuninstall.ps1
@@ -2,10 +2,6 @@
 #   $f='c:\path\to\thisFile.ps1'
 #   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
 
-# If this is an MSI, cleaning up comments is all you need.
-# If this is an exe, change installerType and silentArgs
-# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
-
 Uninstall-BinFile -name 'fop' -path $toolsDir/apache-fop/tools/fop-2.1/fop.bat
 
 ## OTHER HELPERS
